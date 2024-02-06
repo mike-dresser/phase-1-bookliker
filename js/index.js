249748bcb0ea1bdb3331ea27b0109f3d48fb66e0
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-05 20:37:02 -0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+
+    const bookList = document.querySelector('#list');
+    const detailsPanel = document.querySelector('#show-panel');
+
+    fetch('http://localhost:3000/books')
+        .then(response => response.json())
+        .then(books => createList(books))
+
+    function createList(books) {
+        books.forEach(book => {
+            let bookListItem = document.createElement('li');
+            bookListItem.textContent = book.title;
+            bookListItem.addEventListener('click', () => showDetails(book));
+            bookList.append(bookListItem);
+        })
+    }
+
+    function showDetails(book) {
+        detailsPanel.textContent = `${book.title} \br ${book.subtitle} /br ${book.author}`
+    }
+
+
+
+
+});
+
+
