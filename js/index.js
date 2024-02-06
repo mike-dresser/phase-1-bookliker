a552660229921bed28a4e86fd5c8d5f6d7587b4c
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-05 21:04:04 -0500

    User likes displayed

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const bookList = document.querySelector('#list');
-    const detailsPanel = document.querySelector('#show-panel');
+    const detailsPanel = document.querySelector('#book-details');
+
+    const detailImage = detailsPanel.querySelector('img');
+    const detailTitle = detailsPanel.querySelector('h1');
+    const detailSubtitle = detailsPanel.querySelector('h2');
+    const detailAuthor = detailsPanel.querySelector('h3');
+    const detailDescription = detailsPanel.querySelector('p');
+    const userLikeList = detailsPanel.querySelector('ul');
 
     fetch('http://localhost:3000/books')
         .then(response => response.json())
-        .then(books => createList(books))
+        .then(books => {
+            createList(books);
+            showDetails(books[0]);
+        })
 
     function createList(books) {
         books.forEach(book => {
@@ -17,7 +27,23 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function showDetails(book) {
-        detailsPanel.innerHTML = `${book.title} \br ${book.subtitle} /br ${book.author}`
+        detailImage.src = book.img_url;
+        detailTitle.textContent = book.title;
+        detailSubtitle.textContent = book.subtitle;
+        detailDescription.textContent = book.description;
+        detailAuthor.textContent = book.author;
+
+        displayLikes(book);
+    }
+
+    function displayLikes(book) {
+        userLikeList.innerHTML = '';
+        
+        book.users.forEach(user => {
+            let userLike = document.createElement('li');
+            userLike.textContent = user.username;
+            userLikeList.append(userLike);
+        })
     }
 
 
